8d50993c2f12a874c12746f1fdfbfdfaddd3431d
chowdaryiv <chowdaryimmanni@gmail.com>
2025-02-04 16:14:11 +0530

    resolved middleware bug

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 export function middleware(request) {
     const path = request.nextUrl.pathname
     const isPublicPath = path === '/login' || path === '/signup'
-    const token = request.cookies.get('token').value || ""
+    const token = request.cookies.get('token')?.value || ""
 
 
     if (isPublicPath && token) {
